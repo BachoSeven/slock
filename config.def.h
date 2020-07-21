dc4e386faf1235f2feaa78eaa682c877d90a4c1d
Francesco Minnocci <ad17fmin@uwcad.it>
2020-07-21 15:52:41 +0200

    Add blur patch

--- config.def.h
+++ config.def.h
@@ -19,5 +19,14 @@ static const char *colorname[NUMCOLS] = {
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 0;
 
+/*Enable blur*/
+#define BLUR
+/*Set blur radius*/
+static const int blurRadius=5;
+/*Enable Pixelation*/
+//#define PIXELATION
+/*Set pixelation radius*/
+static const int pixelSize=0;
+
 /* time in seconds before the monitor shuts down */
 static const int monitortime = 5;

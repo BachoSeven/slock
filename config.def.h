6284ca68525b574f2d997866bfa7692881b2b94d
Francesco Minnocci <ad17fmin@uwcad.it>
2020-07-22 02:09:51 +0200

    Update colors

--- config.def.h
+++ config.def.h
@@ -9,24 +9,15 @@ static const char *group = "nobody";
 	// [FAILED] = "#dc322f",   [> wrong password <]
 // };
 
-/* Gruvbox */
+/* Gruvbox low-contrast */
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT] =   "#32302f",     /* after initialization */
+	[INPUT] =  "#689d6a",   /* during input */
+	[FAILED] = "#cc241d",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 0;
-
-/*Enable blur*/
-#define BLUR
-/*Set blur radius*/
-static const int blurRadius=5;
-/*Enable Pixelation*/
-//#define PIXELATION
-/*Set pixelation radius*/
-static const int pixelSize=0;
 
 /* time in seconds before the monitor shuts down */
 static const int monitortime = 5;

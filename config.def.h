35bb2da8c93e362ad397fada65b3446a776c63c1
Francesco Minnocci <ad17fmin@uwcad.it>
2020-09-28 00:16:24 +0200

    Update timeout

--- config.def.h
+++ config.def.h
@@ -4,9 +4,9 @@ static const char *group = "nobody";
 
 /* Solarized */
 // static const char *colorname[NUMCOLS] = {
-	// [INIT] =   "#073642",     [> after initialization <]
-	// [INPUT] =  "#2aa198",   [> during input <]
-	// [FAILED] = "#dc322f",   [> wrong password <]
+	// [INIT] =   "#073642",     /* after initialization */
+	// [INPUT] =  "#2aa198",   /* during input */
+	// [FAILED] = "#dc322f",   /* wrong password */
 // };
 
 /* Gruvbox low-contrast */
@@ -20,4 +20,4 @@ static const char *colorname[NUMCOLS] = {
 static const int failonclear = 0;
 
 /* time in seconds before the monitor shuts down */
-static const int monitortime = 5;
+static const int monitortime = 2;
